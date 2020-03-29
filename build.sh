bf09f101dfac58e7acbd652e1108d1ee781b288b
Takashi Sasaki <t.sasaki@bmandw.com>
2020-03-29 15:11:09 +0900

    no apt

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-apt update
-apt install python3 python3-pip liblzma
-python3 -m pip install -U pip
+#apt update
+#apt install python3 python3-pip liblzma
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py --user
 python3 -m pip install -r requirements.txt
 python3 coronavirus_graph.py
 
